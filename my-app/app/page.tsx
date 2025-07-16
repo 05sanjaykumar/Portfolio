cd640bbe5ad757e0366e6a2c285877bca43a70b5
SanjayKumar <0508sanjaykumar@gmail.com>
2025-07-16 13:57:56 +0530

    chore: Updated with packages

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,7 +5,6 @@ import { Footer } from "../components/sections/FooterSection";
 import { HeroSection } from "../components/sections/HeroSection";
 import { ContactSection } from "../components/sections/ContactSection";
 import { ServicesSection } from "@/components/sections/ServicesSection";
-import { Package } from "lucide-react";
 import { PackagesSection } from "@/components/sections/PackageSection";
 
 
